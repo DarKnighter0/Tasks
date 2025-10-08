fe523b4c187de86ccbdffcc3d1a92587fddb2d78
DarKnighter0 <ucik0713@gmail.com>
2025-10-08 06:04:17 +0000

    Task 3

---
+++ Task3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+
+int main() {
+
+	const char *text = "Hello";
+	
+	int lenght = std::strlen(text);
+	
+	std::cout << lenght << std::endl;
+	
+	return 0;
+}
+
